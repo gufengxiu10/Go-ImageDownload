96e65858d60f9da8eb8a9df0705f9446c79415d2
ANNG <annyhanyuu@gmail.com>
2021-11-10 17:56:13 +0800

    下载

--- request/download.go
+++ request/download.go
@@ -15,12 +15,17 @@ func download(url string, path string) {
 	if FileExists(fileName) {
 		return
 	}
-
-	response, err := http.Get(url)
+	client := &http.Client{}
+	reqest, err := http.NewRequest("GET", url, nil)
+	//增加header选项
+	reqest.Header.Add("referer", "https://pixiviz.pwp.app/")
+	reqest.Header.Add("origin", "https://pixiviz.pwp.app")
+	reqest.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44")
 	if err != nil {
 		println(err)
 		return
 	}
+	response, _ := client.Do(reqest)
 	file, _ := os.Create(fileName)
 	defer file.Close()
 	defer response.Body.Close()
--- request/pixiviz.go
+++ request/pixiviz.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 )
 
 //https://pixiviz-api-us.pwp.link/v1/illust/rank?mode=day&date=2021-11-07&page=18
@@ -44,17 +47,29 @@ func (b *PixivizStruct) Rand() {
 	body, _ := ioutil.ReadAll(response.Body)
 	defer response.Body.Close()
 	var tempMap = make(map[string][]map[string]interface{})
-
 	err = json.Unmarshal([]byte(string(body)), &tempMap)
 
 	if err != nil {
 		panic(err)
 	}
 
-	for _, v := range tempMap["illusts"] {
+	// blob:https://pixiviz.pwp.app/ebb6d427-e31f-4c41-852b-e7438aa2dc17
 
-		fmt.Printf("%T\n", v)
-		// var c []interface{} = v["meta_pages"]
-		// fmt.Println(page)
+	for _, v := range tempMap["illusts"] {
+		if len(v["meta_pages"].([]interface{})) == 0 {
+			fmt.Println(10)
+		} else {
+			imageUrls := v["meta_pages"].([]interface{})
+
+			// fmt.Println(strconv.Itoa(int(v["id"].(float64))))
+			path := "image/pixiviz/rank/" + strconv.Itoa(int(v["id"].(float64)))
+			os.MkdirAll(path, 0775)
+			for _, imageV := range imageUrls {
+				url := imageV.(map[string]interface{})["image_urls"].(map[string]interface{})["original"]
+				url = strings.Replace(url.(string), "i.pximg.net", "pixiv-image-lv.pwp.link", 1)
+				fmt.Println(url)
+				download(url.(string), path)
+			}
+		}
 	}
 }
